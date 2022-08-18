857a9475bcf3afc818eb50b7c28601ac0993b10a
irisliu128 <110997839+irisliu128@users.noreply.github.com>
2022-08-18 14:31:12 -0400

    Roni Helping Reco Pushing heehee
    best of luck

--- ReCo/ReCo/TodayUIViewController.swift
+++ ReCo/ReCo/TodayUIViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class TodayUIViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-var imagePicker = UIImagePickerController()
-    
 
+    var imagePicker = UIImagePickerController()
+    
     @IBOutlet weak var imageView: UIImageView!
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
     
         if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
@@ -19,6 +20,7 @@ var imagePicker = UIImagePickerController()
             imagePicker.dismiss(animated: true, completion: nil)
         }
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
@@ -29,16 +31,4 @@ var imagePicker = UIImagePickerController()
         
         present(imagePicker, animated: true, completion: nil)
     }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
